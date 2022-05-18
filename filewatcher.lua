2be91ca9925910b3822fe43b07394e94c43cb2ae
Danilo Mendoza <danilodevs@gmail.com>
2022-05-18 19:59:58 -0400

    Update

---
+++ filewatcher.lua
@@ -0,0 +1,74 @@
+local getTime = love.timer.getTime
+local function merge(source, other)
+    for k, v in pairs(other) do source[k] = v end
+    return source
+end
+
+local function noop() end
+
+-------------------------------------------------------------
+
+local function createFilewatcher(settings)
+    local internal = {
+        timeLastScanned = getTime(),
+        onFileChanged   = noop,
+        scanInterval    = 0.2
+    }
+
+    local watcher = { files = {}, internal = internal }
+
+    -------------------------------------------------------------------
+
+    function watcher.addFile(...)
+        paths = { ... }
+
+        for i, path in ipairs(paths) do
+            if watcher.files[path] then return end
+            local info = love.filesystem.getInfo(path)
+            if not info then error("File '" .. path .. "' does not exist") end
+
+            local file = merge({ path = path }, info)
+            watcher.files[path] = file
+        end
+    end
+
+    function watcher.removeFile(path) table.remove(watcher.files, path) end
+    function watcher.onFileChanged(callback) internal.onFileChanged = callback end
+    function watcher.setScanInterval(time) internal.scanInterval = time end
+
+    -------------------------------------------------------------------
+
+    function watcher.scan()
+        local filesChanged = {}
+
+        for k, file in pairs(watcher.files) do
+            local info = love.filesystem.getInfo(file.path)
+
+            if not info then
+                watcher.files[k] = nil
+            elseif info.modtime ~= file.modtime then
+                merge(file, info) -- Update file info
+                -- internal.onFileChanged(file)
+                return file
+            end
+        end
+    end
+
+  -------------------------------------------------------------------
+
+    function watcher.update(onFileChanged)
+        if getTime() > internal.timeLastScanned then
+        local file = watcher.scan()
+
+        if file then
+            (onFileChanged or noop)(file)
+        end
+
+        internal.timeLastScanned = getTime() + internal.scanInterval
+        end
+    end
+
+    return watcher
+end
+
+return createFilewatcher
--- loveswap.lua
+++ loveswap.lua
@@ -1,364 +1,321 @@
-local trueRequire = require
-local loveswap    = {}
-local filewatcher = (require "lib.loveswap.filewatcher")()
-local createHot   = require "lib.loveswap.hot"
-local function noop() end
+local realcache = {}
+local hotcache = {}
+local ignoreKeysMap = {}
+local trueRequire = _G.require
+local filewatcher = trueRequire("filewatcher")()
+local errorState = nil
+local inError = false
+local errorCapturer
+local varCache = {}
 
-loveswap.global = {}
+local loveswap = {
+    internal = {
+        realcache = realcache,
+        hotcache = hotcache,
+        filewatcher = filewatcher,
+    },
 
----------------------------------------------------------
+    enabled = true,
+    initRun = true,
+}
 
 local loveFuncNames = {
-  "update",
-  "load",
-  "draw",
-  "mousepressed",
-  "mousereleased",
-  "keypressed",
-  "keyreleased",
-  "focus",
-  "quit",
+    update = true,
+    load = true,
+    draw = true,
+    mousepressed = true,
+    mousereleased = true,
+    keyreleased = true,
+    keypressed = true,
+    focus = true,
+    quit = true,
 }
 
----------------------------------------------------------
+local function getModnameFromFile(file)
+    return string.gsub(string.sub(file.path, 1, -5), "[/]", ".")
+end
 
-local defaultIgnorePatterns = { "lib[.]loveswap" }
+local function getFilepathFromModname(modname)
+    return string.gsub(modname, "[.]", "/") .. ".lua"
+end
 
-local internal = {
-  hots        = {},
-  hotStack    = {},
-  errors      = {},
-  require     = require,
-  enabled     = false,
-  cache       = cache,
-  filewatcher = filewatcher,
-  currentHot  = nil,
-  state       = "normal",
-  firstRun    = true,
-
-  ignoreModulePatterns      = { unpack(defaultIgnorePatterns) },
-  currentModulePath         = nil,
-  errorTraceStack           = {},
-  beforeSwapModuleCallbacks = {},
-  afterSwapModuleCallbacks  = {},
-  hotLoveFuncs              = {},
-}
-
-internal.global = {
-  beforeSwapModuleCallback = noop,
-  afterSwapModuleCallback  = noop
-}
-
-loveswap.internal = internal
-filewatcher.addFile("main.lua")
-
----------------------------------------------------------
-
-filewatcher.onFileChanged(function(file)
-  local modulepath = string.gsub(string.sub(file.path, 1, -5), "[/]", ".")
-  loveswap.updateModule(modulepath)
-end)
-
----------------------------------------------------------------------------------
-
-function internal.wrapAndUpdateLoveFuncs() -- TODO: Love functions outside main?
-  if internal.state == "error" then return end
-
-  local onError = function(err)
-    internal.errorTraceStack["main"] = debug.traceback()
-    internal.errors["main"] = err
-    loveswap.error()
-  end
-
-  for i, name in ipairs(loveFuncNames) do
-    local loveFunc = love[name]
-
-    if loveFunc then
-      local hot = internal.hotLoveFuncs[name] or createHot()
-
-      if loveFunc ~= hot.wrapper then
-        hot.update(function(...) xpcall(loveFunc, onError, ...) end)
-      end
-
-      internal.hotLoveFuncs[name] = hot
-      love[name] = hot.wrapper
+local function pairCount(tbl)
+    local count = 0
+    for _, _ in pairs(tbl) do
+        count = count + 1
     end
-  end
+    return count
 end
 
-function internal.onUpdateMain()
-  if internal.state ~= "error" then
-    internal.wrapAndUpdateLoveFuncs()
-  end
+local function firstValueInPair(tbl)
+    for _, v in pairs(tbl) do
+        return v
+    end
 end
 
-
----------------------------------------------------------------------------------
-
-function loveswap.setEnabled(enabled) internal.enabled = enabled end
-function loveswap.setFileScanInterval(t) internal.filewatcher.setScanInterval(t) end
-
-function loveswap.ignoreModules(patterns)
-  loveswap.internal.ignoreModulePatterns = { unpack(defaultIgnorePatterns), unpack(patterns) }
+local function tableContains(tbl, value)
+    for _, v in pairs(tbl) do
+        if v == value then
+            return true
+        end
+    end
+    return false
 end
 
-function loveswap.getHasErrors()
-  for k, v in pairs(internal.errors) do return true end
+------------------------------------------------------------------------------------------
+
+
+local function onLoveFuncError(err)
+    errorCapturer.capture(err, debug.traceback(), "main")
 end
 
----------------------------------------------------------------------------------
+local function swapProtectLoveFuncs()
+    for k, fn in pairs(love) do
+        if loveFuncNames[k] then
+            local _fn = fn
+
+            love[k] = function(...)
+                local ok, result = xpcall(_fn, onLoveFuncError, ...)
+                
+                if ok then 
+                    return result 
+                end
+            end
+        end
+    end
+end
+
+local function updateHotModule(modname, ignoreKeys)
+    package.loaded[modname] = nil
+    ignoreKeys = ignoreKeys or ignoreKeysMap[modname]
+    ignoreKeysMap[modname] = ignoreKeys
+    local module
+
+    local ok = xpcall(function()
+        module = trueRequire(modname)
+    end, function(err)
+        errorCapturer.capture(err, debug.traceback(), modname)
+    end)
+
+    if not ok then
+        return
+    end
+
+    realcache[modname] = module
+
+    if type(module) ~= type(hotcache[modname]) then
+        hotcache[modname] = nil
+    end
+
+    if type(module) == "table" and hotcache[modname] == nil then
+        hotcache[modname] = {}
+
+        if ignoreKeys then 
+            for _, k in ipairs(ignoreKeys) do
+            hotcache[modname][k] = realcache[modname][k]
+            end
+        end
+    end
+
+    if type(module) == "table" and type(hotcache[modname]) == "table" then
+        for k, v in pairs(module) do
+            if (not ignoreKeys) or (ignoreKeys and not tableContains(ignoreKeys, k)) then
+                -- retain equality for func == func
+                if type(v) == "function" and type(hotcache[modname][k]) ~= "function" then
+                    local _m = modname
+                    local _k = k
+    
+                    hotcache[_m][_k] = function(...)
+                        return realcache[_m][_k](...)
+                    end
+                else
+                    hotcache[modname][k] = v
+                end
+            end
+        end
+
+        setmetatable(hotcache[modname], getmetatable(module))
+    end
+    
+    if type(module) == "function" then
+        if not hotcache[modname] or type(hotcache[modname]) ~= "function" then
+            hotcache[modname] = function(...)
+                return realcache[modname](...)
+            end
+        end
+    end
+
+    if modname == "main" then
+        swapProtectLoveFuncs()
+    end
+
+    return hotcache[modname]
+end
+
+local function hotRequire(modname, ignoreKeys)
+    if loveswap.enabled == false or string.match(modname, "loveswap") then
+        return trueRequire(modname)
+    end
+
+    local filepath = getFilepathFromModname(modname)
+    local fileinfo = love.filesystem.getInfo(filepath)
+    if not fileinfo then error("no file:", filepath) end
+
+    local module = hotcache[modname]
+
+    if module == nil then
+        filewatcher.addFile(filepath)
+        return updateHotModule(modname, ignoreKeys)
+    else
+        return module
+    end
+
+    return trueRequire(modname)
+end
+
+------------------------------------------------------------------------------------------
+
+local function createErrorCapturer()
+    local this = { errors = {} }
+    local errors = this.errors
+    local prevLoveFuncs = {}
+    local inErrorState = false
+    local isRuntimeError = false
+    
+    function this.onFileChanged(file)
+        local modname = getModnameFromFile(file)
+
+        local ok = xpcall(updateHotModule, function(err)
+            for _, e in ipairs(errors) do
+                if e.modname == modname then
+                    e.err = err
+                    e.stackTrace = debug.traceback()
+                    print(e.err)
+                    print(e.stackTrace)
+                    break
+                end
+            end
+        end, modname)
+
+        if ok then
+            if modname == "main" then
+                prevLoveFuncs.update = love.update
+                prevLoveFuncs.draw = love.draw
+                love.update = this.updateErrors
+                love.draw = this.drawErrors
+            end
+
+            for i, e in ipairs(errors) do
+                if e.modname == modname then
+                    table.remove(errors, i)
+                    this.errorCountChanged()
+                    break
+                end
+            end
+
+            if modname ~= "main" and #errors == 1 then
+                errors[1] = nil
+                this.errorCountChanged()
+            end
+        end
+    end
+
+    function this.updateErrors()
+        filewatcher.update(this.onFileChanged)
+    end
+
+    function this.drawErrors()
+        local e = errors[#errors]
+        if not e then return end
+        
+        love.graphics.setColor(255, 255, 255)
+        love.graphics.print("(" .. e.modname .. ") " .. (e.err or "error"), 80, 80)
+        love.graphics.print(e.stackTrace or "no stack trace", 80, 100)
+    end
+
+    function this.restorePrevLoveFuncs()
+        for k, _ in pairs(loveFuncNames) do love[k] = nil end
+        for k, fn in pairs(prevLoveFuncs) do love[k] = fn end
+        prevLoveFuncs = {}
+    end
+
+    function this.onErrorStateEnter()
+        prevLoveFuncs = {}
+        for k, _ in pairs(loveFuncNames) do prevLoveFuncs[k] = love[k]; love[k] = nil end
+        love.update = this.updateErrors
+        love.draw = this.drawErrors
+    end
+
+    function this.onErrrorStateExit()
+        this.restorePrevLoveFuncs()
+    end
+
+    function this.errorCountChanged()
+        if not inErrorState and #errors > 0 then
+            inErrorState = true
+            this.onErrorStateEnter()
+
+        elseif inErrorState and #errors == 0 then
+            inErrorState = false
+            this.onErrrorStateExit()
+        end
+    end
+
+    function this.capture(err, stackTrace, modname)
+        print(err)
+        print(stackTrace)
+
+        local e = {
+            err        = err,
+            stackTrace = stackTrace,
+            modname    = modname,
+        }
+
+        table.insert(errors, e)
+        this.errorCountChanged()
+    end
+
+    return this
+end
+
+errorCapturer = createErrorCapturer()
+
+--------------------------------------------------------------------------------------------
+
+local function onFileChanged(file)
+    local modname = getModnameFromFile(file)
+    updateHotModule(modname)
+end
+
+filewatcher.addFile("main.lua")
+loveswap.require = hotRequire
 
 function loveswap.update()
-  if not internal.enabled then return end
+    if loveswap.initRun == true then
+        loveswap.initRun = false
+        swapProtectLoveFuncs()
 
-  if internal.firstRun then
-    internal.firstRun = false
-    internal.wrapAndUpdateLoveFuncs()
-  end
-
-  internal.filewatcher.update()
-end
-
----------------------------------------------------------------------------------
-
-function loveswap.updateModule(modulepath)
-  local hotModule, moduleValue = internal.hots[modulepath]
-  if not hotModule then
-    hotModule = createHot()
-    internal.hots[modulepath] = hotModule
-  end
-
-  internal.currentModulePath = modulepath
-  ;(internal.beforeSwapModuleCallbacks[modulepath] or noop)(modulepath);
-  internal.global.beforeSwapModuleCallback(modulepath)
-
-  local loadedBackup = package.loaded[modulepath]
-  package.loaded[modulepath] = nil
-  internal.currentHot = hotModule
-
-  local ok = xpcall(function()
-    moduleValue = trueRequire(modulepath)
-  end, function(err)
-    internal.errorTraceStack[modulepath] = debug.traceback()
-    internal.errors[modulepath] = err
-    loveswap.error()
-  end)
-
-  if not ok then
-    package.loaded[modulepath] = loadedBackup
-    internal.currentHot = nil
-    internal.currentModulePath = nil
-    return hotModule
-  end
-
-
-  internal.errors[modulepath] = nil
-  if not loveswap.getHasErrors() then
-    internal.state = "normal"
-    if modulepath == "main" then internal.wrapAndUpdateLoveFuncs() end
-  end
-
-  hotModule.update(moduleValue)
-  for name, hot in pairs(internal.hotStack) do hotModule.addChild(name, hot) end
-
-  ---------------------------------------------------------------------------
-
-  internal.hotStack = {}
-  internal.currentHot = nil
-  internal.currentModulePath = nil
-  ;(internal.afterSwapModuleCallbacks[modulepath] or noop)(modulepath);
-  internal.global.afterSwapModuleCallback(modulepath)
-  return hotModule
-end
-
----------------------------------------------------------------------------------
-
-local function hasIgnoredPattern(modulepath)
-  for i, pattern in ipairs(internal.ignoreModulePatterns) do
-    if string.match(modulepath, pattern) then return true end
-  end
-end
-
-function loveswap.require(modulepath)
-  if not internal.enabled then return trueRequire(modulepath) end
-  if hasIgnoredPattern(modulepath) then return trueRequire(modulepath) end
-  if internal.hots[modulepath] then return internal.hots[modulepath].wrapper end
-  local filepath = string.gsub(modulepath, "[.]", "/") .. ".lua"
-  local fileinfo = love.filesystem.getInfo(filepath)
-  if not fileinfo then error("no file:", filepath) end
-
-  internal.filewatcher.addFile(filepath)
-
-  --------------------------------------------------------------
-
-  local hot = internal.hots[modulepath]
-
-  if not hot then
-    hot = loveswap.updateModule(modulepath)
-    internal.hots[modulepath] = hot
-  end
-
-  return hot.wrapper
-end
-
----------------------------------------------------------------------------------
-
-local function createHotFunction(func)
-  local hot = createHot()
-  hot.update(func, true)
-  return hot
-end
-
-local function createHotTable(tbl)
-  local hot = createHot()
-  hot.update(tbl, true)
-  return hot
-end
-
---------------------------------------------------------------------------------
-
----@generic T
----@param name string
----@param value T
----@param flags table
----@return T
-function loveswap.hot(name, value, flags)
-  if not internal.enabled then return value end
-  if value == nil or not name then return end
-  if type(value) ~= "table" and type(value) ~= "function" then return end
-  local currentHot, hotTarget = internal.currentHot
-  flags = flags or { skip = false }
-
-  if not currentHot then -- Module does not exist yet, need to initiate
-    hotTarget = type(value) == "function"
-      and createHotFunction(value)
-      or createHotTable(value)
-  else
-    hotTarget = currentHot.children[name]
-
-    if not hotTarget then
-      hotTarget = type(value) == "function"
-        and createHotFunction(value)
-        or createHotTable(value)
-    end
-
-    hotTarget.setFlags(flags)
-    hotTarget.update(value)
-  end
-
-  hotTarget.setFlags(flags)
-  internal.hotStack[name] = hotTarget
-  return hotTarget.wrapper
-end
-
---------------------------------------------------------------------------------
-
----@generic T
----@param name string
----@param target T
----@return T
-function loveswap.skip(name, target)
-  return loveswap.hot(name, target, { skip = true })
-end
-
----@generic T
----@param name string
----@param target T
----@return T
-function loveswap.unskip(name, target)
-  return loveswap.hot(name, target, { skip = false })
-end
-
-function loveswap.once(name, fn)
-  return loveswap.hot(name, fn, { once = true })
-end
-
---------------------------------------------------------------------------------
-
--- TODO: Remove callback on file when updating
-do -- before/after module swap callback hook
-  local function checkModulePathExists()
-    if not internal.currentModulePath then error("Not called in module instantiation") end
-  end
-
-  function love.beforeModuleSwap(callback)
-    checkModulePathExists()
-    internal.beforeSwapModuleCallbacks[internal.currentModulePath] = callback
-  end
-
-  function love.afterModuleSwap(callback)
-    checkModulePathExists()
-    internal.afterSwapModuleCallbacks[internal.currentModulePath] = callback
-  end
-end
-
---------------------------------------------------------------------------------
-
-function loveswap.global.beforeModuleSwap(callback)
-  internal.global.beforeSwapModuleCallback = callback or noop
-end
-
-function loveswap.global.afterModuleSwap(callback)
-  internal.global.afterSwapModuleCallback = callback or noop
-end
-
---------------------------------------------------------------------------------
-
-
-do
-  local errToShow = ""
-  local errModulePath = nil
-  local prevModulepath = nil
-
-  local function errUpdate()
-    if not loveswap.getHasErrors() then
-      internal.state = "normal"
-      loveswap.revertLoveFuncsFromError()
+        if loveswap.firstRun ~= nil then
+            loveswap.firstRun()
+        end
     else
-      loveswap.error()
+        filewatcher.update(onFileChanged)
     end
-
-    filewatcher.update()
-  end
-
-  local errDraw = function()
-    if internal.state ~= "error" then return end
-    love.graphics.setColor(255, 255, 255)
-    love.graphics.print(errToShow or "Error: check console", 80, 80)
-    love.graphics.print(internal.errorTraceStack[errModulePath] or "", 80, 100)
-  end
-
-  function loveswap.error()
-    if not loveswap.getHasErrors() then
-      prevModulepath = nil
-      return
-    end
-
-    if internal.state ~= "error" then
-      internal.state = "error"
-      prevModulepath = tostring(math.random())
-
-      for i, name in ipairs(loveFuncNames) do love[name] = noop end
-    end
-
-    love.update = errUpdate
-    love.draw = errDraw
-
-    for modulepath, err in pairs(internal.errors) do
-      errModulePath = modulepath
-      errToShow = err
-      if prevModulepath ~= modulepath then print(err) print(internal.errorTraceStack[modulepath]) end
-      prevModulepath = modulepath
-      break
-    end
-  end
-
-  function loveswap.revertLoveFuncsFromError()
-    prevModulepath = tostring(math.random())
-    for name, hot in pairs(internal.hotLoveFuncs) do love[name] = hot.wrapper end
-  end
 end
 
---------------------------------------------------------------------------------
+---@generic T
+---@param name string
+---@param seed T
+---@return T
+function loveswap.cached(name, seed)
+    local value = varCache[name]
+
+    if value == nil then
+        value = seed
+        varCache[name] = value
+    end
+
+    return value
+end
 
 return loveswap
